b5b899d32945547a4703eff439248851cbf849bd
ouazizi <158299018+helouazizi@users.noreply.github.com>
2024-11-30 17:37:57 +0100

    restart the  instarctions

--- lemin/main.go
+++ lemin/main.go
@@ -6,184 +6,53 @@ import (
 	"container/list"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
-// Room struct to hold room information
+// this struct represent a room
+// with it name and coordonetes (x,y)
 type Room struct {
 	Name string
 	X    int
 	Y    int
 }
 
-// Graph struct to hold rooms and links
+// this struct reperesent an undirect graph
+// data structure to holde rooms as a map
+// and links between them as a map
 type Graph struct {
 	Rooms map[string]*Room
 	Links map[string]*list.List
 }
 
-// AddRoom method to add a room to the graph
-func (g *Graph) AddRoom(name string, x int, y int) {
-	g.Rooms[name] = &Room{Name: name, X: x, Y: y}
-}
-
-// AddLink method to add a link between two rooms
-func (g *Graph) AddLink(from string, to string) {
-	if g.Links[from] == nil {
-		g.Links[from] = list.New()
-	}
-	g.Links[from].PushBack(to)
-}
-
-// BFS method to find the path from start to end room
-func (g *Graph) BFS(start string, end string) []string {
-	visitedRooms := make(map[string]bool)
-	result := []string{}
-	nodesQueue := list.New()
-
-	nodesQueue.PushBack(start)
-	visitedRooms[start] = true
-
-	for nodesQueue.Len() > 0 {
-		currentRoom := nodesQueue.Front().Value.(string)
-		nodesQueue.Remove(nodesQueue.Front())
-		result = append(result, currentRoom)
-
-		// Check if there are links for the current room
-		if g.Links[currentRoom] == nil {
-			continue // Skip if no links
-		}
-
-		for e := g.Links[currentRoom].Front(); e != nil; e = e.Next() {
-			neighbor := e.Value.(string)
-			if !visitedRooms[neighbor] {
-				nodesQueue.PushBack(neighbor)
-				visitedRooms[neighbor] = true
-			}
-		}
-	}
-
-	return result
-}
-
-// readInput function to read graph data from a file
-func readInput(filename string) (*Graph, error) {
-	graph := &Graph{
-		Rooms: make(map[string]*Room),
-		Links: make(map[string]*list.List),
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	// Read number of ants
-	/*var numAnts int
-	if scanner.Scan() {
-		numAnts, _ = strconv.Atoi(scanner.Text())
-	}*/
-
-	var startRoomName, endRoomName string
-
-	// Read rooms
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "##start" {
-			scanner.Scan()
-			startRoomData := strings.Fields(scanner.Text())
-			if len(startRoomData) == 3 {
-				startRoomName = "start" // Assign a name for the start room
-				x, _ := strconv.Atoi(startRoomData[0])
-				y, _ := strconv.Atoi(startRoomData[1])
-				graph.AddRoom(startRoomName, x, y)
-			}
-			continue
-		} else if line == "##end" {
-			scanner.Scan()
-			endRoomData := strings.Fields(scanner.Text())
-			if len(endRoomData) == 3 {
-				endRoomName = "end" // Assign a name for the end room
-				x, _ := strconv.Atoi(endRoomData[0])
-				y, _ := strconv.Atoi(endRoomData[1])
-				graph.AddRoom(endRoomName, x, y)
-			}
-			continue
-		}
-
-		// Read other rooms
-		roomData := strings.Fields(line)
-		if len(roomData) == 3 {
-			roomName := roomData[0]
-			x, _ := strconv.Atoi(roomData[1])
-			y, _ := strconv.Atoi(roomData[2])
-			graph.AddRoom(roomName, x, y)
-			continue
-		}
-
-		// Read links
-		if strings.Contains(line, "-") {
-			parts := strings.Split(line, "-")
-			if len(parts) == 2 {
-				graph.AddLink(parts[0], parts[1])
-			}
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return graph, nil
-}
-
 func main() {
+	// lest check the argements
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: lem-in <input_file>")
+		fmt.Println("[Usage]: ./lemin exemple.txt")
 		os.Exit(1)
 	}
 
 	filename := os.Args[1]
-	graph, err := readInput(filename)
+
+	// lets read the file
+	graph, numOfants, err := readfile(filename)
+}
+
+func readfile(filename string) (*Graph, int, error) {
+	// lets open the file
+	// and read it line by line
+	// we will store the rooms and links in the graph struct
+	// we will also count the number of ants
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
-		os.Exit(1)
+		return nil, 0, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	// we will read the file line by line
+	// the first line is the number of ants
+	for scanner.Scan() {
+		line := scanner.Text()
+		
 	}
 
-	startRoom := graph.Rooms["start"]
-	endRoom := graph.Rooms["end"]
-
-	if startRoom == nil || endRoom == nil {
-		fmt.Println("ERROR: no start or end room found")
-		os.Exit(1)
-	}
-
-	fmt.Println("Starting BFS...")
-
-	// Debugging: Print the rooms and links
-	fmt.Println("Rooms:")
-	for _, room := range graph.Rooms {
-		fmt.Printf("Name: %s, Coordinates: (%d, %d)\n", room.Name, room.X, room.Y)
-	}
-
-	fmt.Println("Links:")
-	for from, links := range graph.Links {
-		fmt.Printf("%s-", from)
-		for e := links.Front(); e != nil; e = e.Next() {
-			fmt.Printf(" %s", e.Value.(string))
-		}
-		fmt.Println()
-	}
-
-	path := graph.BFS(startRoom.Name, endRoom.Name)
-
-	fmt.Printf("Path length: %d\n", len(path))
-	for _, room := range path {
-		fmt.Printf("%s ", graph.Rooms[room].Name)
-	}
-	fmt.Println()
 }
--- main.go
+++ main.go
@@ -1,89 +1,18 @@
-// main.go
 package main
 
-import (
-	"container/list"
-	"fmt"
-)
+import "fmt"
 
-// Graph represents an undirected graph
-type Graph struct {
-	adjacencyList map[int]*list.List
-}
+func main() {
+	array := [5]int{9, 3, 6, 4, 5}
 
-// NewGraph creates a new empty graph
-func NewGraph() *Graph {
-	return &Graph{
-		adjacencyList: make(map[int]*list.List),
-	}
-}
-
-// AddVertex adds a new vertex to the graph
-func (g *Graph) AddVertex(v int) {
-	g.adjacencyList[v] = list.New()
-}
-
-// AddEdge adds an edge between two vertices
-func (g *Graph) AddEdge(v1, v2 int) {
-	// check if the vertex already exist
-	// if not add a vertex
-	if g.adjacencyList[v1] == nil {
-		g.adjacencyList[v1] = list.New()
-	}
-	if g.adjacencyList[v2] == nil {
-		g.adjacencyList[v2] = list.New()
-	}
-	g.adjacencyList[v1].PushBack(v2)
-	g.adjacencyList[v2].PushBack(v1)
-}
-
-// BFS performs a breadth-first search starting from the given vertex
-func BFS(g *Graph, start int) []int {
-	// A map to track which vertices have been visited to avoid cycles.
-	visited := make(map[int]bool)
-	result := []int{}
-	queue := list.New()
-
-	// Mark the start node as visited and enqueue it
-	visited[start] = true
-	queue.PushFront(start)
-	// The loop continues until there are no more vertices in the queue.
-	for queue.Len() > 0 {
-		// get the first element in the list with it value
-		current := queue.Front().Value.(int)
-		// after that remove it from the list
-		queue.Remove(queue.Front())
-		// If the current vertex is not already in the result, it is added.
-
-		if len(result) == 0 || current != result[len(result)-1] {
-			result = append(result, current)
-		}
-
-		// Dequeue all nodes adjacent to this node
-		for neighborElement := g.adjacencyList[current].Front(); neighborElement != nil; neighborElement = neighborElement.Next() {
-			neighbor := neighborElement.Value.(int)
-			if !visited[neighbor] {
-				visited[neighbor] = true
-				queue.PushFront(neighbor)
+	n := len(array)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if array[i] > array[j] {
+				array[i], array[j] = array[j], array[i]
 			}
 		}
 	}
+	fmt.Println(array)
 
-	return result
-}
-
-func main() {
-	graph := NewGraph()
-	graph.AddVertex(0)
-	graph.AddVertex(1)
-	graph.AddVertex(2)
-	graph.AddVertex(3)
-	graph.AddVertex(4)
-
-	graph.AddEdge(0, 1)
-	graph.AddEdge(0, 2)
-	graph.AddEdge(1, 3)
-	graph.AddEdge(2, 4)
-
-	fmt.Println("BFS traversal:", BFS(graph, 0))
 }
