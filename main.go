9132a8d1812933b19bd2cedccf32254ca30d1750
hassan el ouazizi <ouazizi2code@gmail.com>
2025-01-18 20:55:57 +0100

    setup a couple of methods to study

--- main.go
+++ main.go
@@ -1,34 +1,19 @@
 // main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	array := make([]int, 1)
-	/*n := len(array)
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if array[i] > array[j] {
-				array[i], array[j] = array[j], array[i]
-			}
-		}
-	}*/
-	mapp := make(map[string]int)
-	fmt.Println(len(mapp))
-	fmt.Println(cap(array))
-	fmt.Println(len(array))
-	fmt.Println(array)
-	array = append(array, 6, 5, 6, 6, 6, 6)
-	fmt.Println(cap(array))
-	fmt.Println(len(array))
+	fmt.Println(IsPrime(-25))
+}
 
-	fmt.Println(array)
-	p := new([]int)
-	fmt.Println(cap(*p))
-	fmt.Println(len(*p))
-	*p = append(*p, 5,5)
-	fmt.Println(*p)
-	fmt.Println(cap(*p))
-	fmt.Println(len(*p))
-
+func IsPrime(n int) bool {
+	fmt.Println((math.Sqrt(float64(n))))
+	if n <= 1 {
+		return false
+	}
+	return true
 }
