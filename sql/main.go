984a70d3ebfd41f3847067b3bb2be77f8a909f67
helouazizi <ouazizi2code@gmail.com>
2025-02-10 20:21:11 +0100

    add login handler

--- sql/main.go
+++ sql/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Database struct {
+	*sql.DB
+}
+
+func New_DB() (*Database, error) {
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		return nil, err
+	}
+	return &Database{db}, nil
+}
 
 func main() {
-	fmt.Println("hassan using sqlite")
+	db, err := New_DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (username, password)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("database created seccusfully")
+	http.HandleFunc("/login", Login)
+	fmt.Println("http://localhost:8080/login")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not allowed\n"))
+		return
+	}
+
+	r.ParseForm()
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println(username, password)
+	db, err := New_DB()
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal server error\n"))
+		return
+	}
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES (?,?)", username, password)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal server error\n"))
+		return
+	}
+	var userName string
+	err = db.QueryRow("SELECT username FROM users WHERE username = ?", username).Scan(&userName)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal server error\n"))
+		return
+	}
+	w.Write([]byte("hello from server\n"))
 }
