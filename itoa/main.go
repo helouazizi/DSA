786c4e1286961f6bc3058069cc3bda8140f20f85
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-15 13:52:44 +0100

    add some

--- itoa/main.go
+++ itoa/main.go
@@ -4,12 +4,9 @@ import "fmt"
 
 func main() {
 	num := 55
-	text := "111"
 	num_as_text := Itoa(num)
-	inttext := Atoi(text)
 	fmt.Printf("num:%d %T\n", num, num)
 	fmt.Printf("num_as_text:%s %T\n", num_as_text, num_as_text)
-	fmt.Printf("inttext:%d %T\n", inttext, inttext)
 }
 
 func Itoa(num int) string {
@@ -20,12 +17,4 @@ func Itoa(num int) string {
 		num /= 10
 	}
 	return num_as_text
-}
-
-func Atoi(num string) int {
-	intt := 0
-	for _, v := range num {
-		intt = intt*10 + (int(v) - '0')
-	}
-	return intt
 }
