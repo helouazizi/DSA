75cb8873d602996f0d972e72fe3459470b70e919
helouazizi <ouazizi2code@gmail.com>
2025-02-17 14:48:01 +0100

    update list

---
+++ graph/linkedlist.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+typedef struct Node
+{
+    int Vertex;
+    struct Node* next;
+    
+}Node;
+
+typedef  struct Graph 
+{
+    int Vertexs;
+    Node* list;
+
+}Graph;
+
+
+
+
+int main(){
+    puts("here list");
+}
