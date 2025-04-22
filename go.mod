eed8a766a2454211e43b04c157b838c30f38b3a2
helouaziz <ouazizi2code@gmail.com>
2025-04-22 10:20:44 +0100

    print-memory

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dsa
 
 go 1.23.5
+
+require github.com/01-edu/z01 v0.1.0
--- print-memory/main.go
+++ print-memory/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
+func main() {
+	test := [10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'}
+	PrintMemory(test)
+	// z01.PrintRune('h')
+}
 
-
-func main()  {
-	fmt.Println("yes")
+func PrintMemory(text [10]byte) {
+	hex := "0123456789abcdef"
+	for i, char := range text {
+		z01.PrintRune(rune(hex[char>>4]))   // High nibble (first hex digit)
+		
+		z01.PrintRune(rune(hex[char&0x0F])) // Low nibble (second hex digit)
+		z01.PrintRune(' ')
+		if (i+1 )% 4 == 0 {
+			z01.PrintRune('\n')
+		}
+	}
 }
