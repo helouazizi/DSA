4a026331970dcd8514461761890bf6e60a3fe023
helouaziz <ouazizi2code@gmail.com>
2025-04-22 10:23:58 +0100

    add print memmory

--- print-memory/main.go
+++ print-memory/main.go
@@ -11,12 +11,21 @@ func main() {
 func PrintMemory(text [10]byte) {
 	hex := "0123456789abcdef"
 	for i, char := range text {
-		z01.PrintRune(rune(hex[char>>4]))   // High nibble (first hex digit)
-		
+		z01.PrintRune(rune(hex[char>>4])) // High nibble (first hex digit)
+
 		z01.PrintRune(rune(hex[char&0x0F])) // Low nibble (second hex digit)
 		z01.PrintRune(' ')
-		if (i+1 )% 4 == 0 {
+		if (i+1)%4 == 0 {
 			z01.PrintRune('\n')
 		}
 	}
+	z01.PrintRune('\n')
+	for _, v := range text {
+		if v <= 127 && v >= 32 {
+			z01.PrintRune(rune(v))
+		} else {
+			z01.PrintRune('.')
+		}
+	}
+	z01.PrintRune('\n')
 }
