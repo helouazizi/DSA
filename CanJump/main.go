0d97d52daf8afc771001fcaa587b5d556118834c
helouazizi <ouazizi2code@gmail.com>
2025-05-20 20:25:05 +0100

    just kiding

--- CanJump/main.go
+++ CanJump/main.go
@@ -25,25 +25,8 @@ func CanJump(input []uint) bool {
 		return true
 	}
 
-	n := len(input)
-	visited := make([]bool, n)
-	queue := []int{0}
-	visited[0] = true
-
-	for len(queue) > 0 {
-		pos := queue[0]
-		queue = queue[1:]
-		// input1 := []uint{2, 3, 1, 1, 4}
-		if int(input[pos])+pos == n-1 {
-			return true
-		}
-
-		next := pos + int(input[pos])
-		if next < n && !visited[next] {
-			visited[next] = true
-			queue = append(queue, next)
-		}
-	}
+	n:= len(input)
+	
 
 	return false
 
