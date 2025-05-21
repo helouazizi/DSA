469140cd6cee42af44f80bbde9cffccce428432c
helouazizi <ouazizi2code@gmail.com>
2025-05-21 09:45:24 +0100

    refactor CanJump function to simplify logic and improve performance

--- CanJump/main.go
+++ CanJump/main.go
@@ -15,35 +15,17 @@ func main() {
 	fmt.Println(CanJump(input3))
 }
 
-// lest code
-
 func CanJump(input []uint) bool {
 	if len(input) == 0 {
 		return false
 	}
-	if len(input) == 1 {
-		return true
-	}
-
-	n := len(input)
-	visited := make([]bool, n)
-	queue := []int{0}
-	visited[0] = true
-	// stope give me seggustion until i finish
-	for len(queue) > 0 {
-		index := queue[0]
-		queue = queue[1:]
-		if index+int(input[index]) == n-1 {
-			return true
+	pos := 0
+	for pos < len(input)-1 {
+		step := int(input[pos])
+		if step == 0 || pos+step >= len(input) {
+			return false
 		}
-
-		next := index + int(input[index])
-		if next < n && !visited[next]{
-			visited[next] = true
-			queue = append(queue, next)
-		}
+		pos += step
 	}
-
-	return false
-
+	return pos == len(input)-1
 }
